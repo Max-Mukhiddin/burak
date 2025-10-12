3a456449f258666420df5ef2ae7efad56e9ed91a
Max-Mukhiddin <@mukhiddinsolijonov101@gmail.com>
2025-10-12 16:20:30 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,7 @@
-// console.log("executed!");
+import dotenv from "dotenv";
+dotenv.config();
 
-// import moment from 'moment';
+console.log("PORT:", process.env.PORT);
 
-// const currentTime = moment().format('YYYY MM DD');
-// console.log(currentTime);
-
-// const person: string = "Martin";
-// const count: number = 100;
-
+console.log("MONGO_URL:", process.env.MONGO_URL);
 
--- train.ts
+++ train.ts
@@ -1,21 +1,23 @@
-function getHighestIndex(arr: number[]): number {
-  if (arr.length === 0) return -1; // bo‘sh array bo‘lsa, -1 qaytaradi
+// MIT TASK 
 
-  let max: number = arr[0];
-  let index: number = 0;
+// function getHighestIndex(arr: number[]): number {
+//   if (arr.length === 0) return -1; // bo‘sh array bo‘lsa, -1 qaytaradi
 
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
-      index = i;
-    }
-  }
+//   let max: number = arr[0];
+//   let index: number = 0;
 
-  return index;
-}
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       max = arr[i];
+//       index = i;
+//     }
+//   }
 
-// Test
-console.log(getHighestIndex([5, 21, 12, 21, 8])); // 1
-console.log(getHighestIndex([3, 7, 2, 1])); // 1
-console.log(getHighestIndex([10, 10, 9])); // 0
-console.log(getHighestIndex([])); // -1
+//   return index;
+// }
+
+// // Test
+// console.log(getHighestIndex([5, 21, 12, 21, 8])); // 1
+// console.log(getHighestIndex([3, 7, 2, 1])); // 1
+// console.log(getHighestIndex([10, 10, 9])); // 0
+// console.log(getHighestIndex([])); // -1
