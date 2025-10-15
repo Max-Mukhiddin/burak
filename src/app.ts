718bd30c0dd7c425cb87a51a38ba95b7289bfa83
Max-Mukhiddin <@mukhiddinsolijonov101@gmail.com>
2025-10-15 13:57:55 +0900

    feat: create routing and member controller

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import path from "path";
+import router from "./router";
 
 /** 1-ENTERANCE **/
 const app = express();
-console.log("__dirname:", __dirname);
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,5 +16,6 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 /** 4-ROUTERS **/
+app.use("/", router);   // Middleware Design Paattern
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,13 @@ dotenv.config();
 import mongoose from "mongoose";
 import app from "./app";
 
-
 mongoose
   .connect(process.env.MONGO_URL as string, {})
   .then((data) => {
     console.log("MongoDB connection succeed");
     const PORT = process.env.PORT ?? 3003;
     app.listen(PORT, function () {
-        console.log(`The server is running successfully on port: ${PORT}`);
+      console.log(`The server is running successfully on port: ${PORT}`);
     });
   })
   .catch((err) => console.log("Error on connection MongoDB", err));
